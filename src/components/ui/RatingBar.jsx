650806912c9d16b74cf85ef767eda01535d17a5b
guerkankaraduman-prog <guerkan.karaduman@gmail.com>
2026-05-15 09:44:23 +0200

    Create RatingBar.jsx

---
+++ src/components/ui/RatingBar.jsx
@@ -0,0 +1,14 @@
+import RatingBar from '../ui/RatingBar'
+
+export default function Hero() {
+  return (
+    <section className="hero">
+      
+      <RatingBar />
+
+      <h1>Authentische italienische Küche</h1>
+      <p>Feinste Pizza & Pasta in Premium-Atmosphäre</p>
+
+    </section>
+  )
+}
