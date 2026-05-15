2c898e7e3f441634f5fb86bc98d4aeb16461645c
Gürkan Karaduman <DEINE-EMAIL>
2026-05-15 10:30:06 +0200

    fix: Logo lokal in Navbar, aus Hero entfernt

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,20 +3,28 @@ import { Link, NavLink, useLocation } from 'react-router-dom'
 import './Navbar.css'
 import logo from '../assets/logo.webp'
 
+const LOGO = logo
+
 export default function Navbar() {
+  const [scrolled, setScrolled] = useState(false)
   const [open, setOpen] = useState(false)
   const { pathname } = useLocation()
 
   useEffect(() => {
-    setOpen(false)
-  }, [pathname])
+    const handleScroll = () => setScrolled(window.scrollY > 60)
+    window.addEventListener('scroll', handleScroll, { passive: true })
+    return () => window.removeEventListener('scroll', handleScroll)
+  }, [])
+
+  useEffect(() => { setOpen(false) }, [pathname])
+
+  const isHome = pathname === '/'
 
   return (
-    <header className={`navbar navbar--solid ${open ? 'navbar--open' : ''}`}>
+    <header className={`navbar ${scrolled || !isHome ? 'navbar--solid' : ''} ${open ? 'navbar--open' : ''}`}>
       <div className="navbar__inner">
-
         <Link to="/" className="navbar__logo">
-          <img src={logo} alt="Mio Naabucco" />
+          <img src={LOGO} alt="Mio Naabucco" />
         </Link>
 
         <nav className={`navbar__nav ${open ? 'navbar__nav--visible' : ''}`}>
@@ -24,16 +32,10 @@ export default function Navbar() {
           <NavLink to="/speisekarte">Speisekarte</NavLink>
           <NavLink to="/kontakt">Kontakt</NavLink>
           <NavLink to="/anfahrt">Anfahrt</NavLink>
-
           <a href="tel:+499435307373" className="navbar__cta btn btn-primary">
             <span>☎ Jetzt Bestellen</span>
           </a>
         </nav>
-
-        <button className="navbar__burger" onClick={() => setOpen(!open)}>
-          ☰
-        </button>
-
       </div>
     </header>
   )
